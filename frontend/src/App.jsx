457d86a4831fa5299acd393eb951862f6e5eae5e
faridardila <deardilah@unal.edu.co>
2025-11-01 15:22:01 -0500

    ahora se puede cambiar entre vistas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import React, { useState } from 'react'
 import { MobileHome } from './Components/CommonUserView'
+import Dashboard from './Components/OperatorView/Dashboard'
 
 function App() {
-  return <MobileHome />
+  const [view, setView] = useState('common') 
+  return (
+    <>
+      <button onClick={() => setView('common')}>Usuario</button>
+      <button onClick={() => setView('operator')}>Operador</button>
+
+      {view === 'common' ? <MobileHome /> : <Dashboard />}
+    </>
+  )
 }
 
 export default App
